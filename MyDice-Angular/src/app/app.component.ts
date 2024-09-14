987864ef2711d9a3bcf41c117e5045c55fc9eeb5
Alejandro Garzon <garzonba@sheridancollege.ca>
2024-09-14 17:00:13 -0400

    Added dice images and created app functionality

--- MyDice-Angular/src/app/app.component.ts
+++ MyDice-Angular/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "./header/header.component";
 import {FooterComponent} from "./footer/footer.component";
+import {RollData} from "./roll-data";
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,41 @@ import {FooterComponent} from "./footer/footer.component";
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  diceValues: number[] = []
+  totalValues: number = 0
 
+  private getRandomDiceValue(): number {
+    return 1 + Math.floor(6 * Math.random())
+  }
+
+  getRollData(numberOfDice: number) {
+    const values: number[] = []
+    let total: number = 0
+
+    for (let i: number = 0; i < numberOfDice; i++) {
+      let diceValue: number = this.getRandomDiceValue()
+      values.push(diceValue)
+      total += diceValue
+    }
+
+    return {
+      numberOfDice: numberOfDice,
+      values: values,
+      total: total
+    }
+  }
+
+  onRollDice(numberOfDice: number) {
+    let rollData: RollData = this.getRollData(numberOfDice)
+
+    this.diceValues = rollData.values
+    this.totalValues = rollData.total
+  }
+
+  onReset() {
+    this.diceValues = []
+    this.totalValues = 0
+  }
+
+  protected readonly parseInt = parseInt;
 }
