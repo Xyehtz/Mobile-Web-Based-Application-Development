da0b4defb291073da7b365b36eb06e2f4b7bc761
Alejandro Garzon <alejitogarbo@gmail.com>
2024-09-28 20:17:56 -0400

    completed card component and added css

--- Assignment1/src/app/cards/cards.component.ts
+++ Assignment1/src/app/cards/cards.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgForOf, NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-cards',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgOptimizedImage
+  ],
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.css'
 })
 export class CardsComponent {
+  imageFiles: string[] = [];
 
+  constructor() {
+    const maxCardNum: number = 7;
+
+    for (let i: number = 2; i <= maxCardNum; i++) {
+      this.imageFiles.push(`../cards/card_${i}.png`);
+    }
+  }
 }
