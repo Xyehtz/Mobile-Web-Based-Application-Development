2d9e7a201c9be0b0ab29ba49802810e8f7a181ec
Alejandro Garzon <alejitogarbo@gmail.com>
2024-10-05 13:46:08 -0400

    added functionality to the buttons

--- Assignment1/src/app/cards/cards.component.ts
+++ Assignment1/src/app/cards/cards.component.ts
@@ -13,12 +13,32 @@ import {NgForOf, NgOptimizedImage} from "@angular/common";
 })
 export class CardsComponent {
   imageFiles: string[] = [];
+  cardNum: number = 5;
 
   constructor() {
-    const maxCardNum: number = 7;
-
-    for (let i: number = 2; i <= maxCardNum; i++) {
-      this.imageFiles.push(`../cards/card_${i}.png`);
+    for (let i: number = 0; i <= this.cardNum; i++) {
+      this.imageFiles.push(`../cards/card_${i + 2}.png`);
     }
+  }
+
+  sortImages(): void {
+    this.imageFiles.splice(0, this.cardNum + 1)
+
+    for (let i: number = 0; i <= this.cardNum; i++) {
+      this.imageFiles.push(`../cards/card_${i + 2}.png`)
+    }
+  }
+
+  shuffleImages(): void {
+    this.imageFiles.splice(0, this.cardNum + 1)
+
+    for (let i: number = 0; i <= this.cardNum; i++) {
+      const randNum: number = Math.floor(Math.random() * (7 - 2) + 2)
+      this.imageFiles[i] = `../cards/card_${randNum}.png`
+    }
+  }
+
+  reverseImages(): void {
+    this.imageFiles.reverse()
   }
 }
