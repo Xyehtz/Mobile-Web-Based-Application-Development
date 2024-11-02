3e308f5e63286599f6260bf73a279ce9404c5c7c
Alejandro Garzon <alejitogarbo@gmail.com>
2024-11-02 19:36:37 -0400

    created input fields for service cost and service quality

--- Assignment2/src/app/form/form.component.ts
+++ Assignment2/src/app/form/form.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import {FormsModule, NgForm} from '@angular/forms';
+import {KeyValuePipe, NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NgIf,
+    NgForOf,
+    KeyValuePipe
+  ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+  serviceQualityOptions: {[key: string]: number} = {
+    "Okay (15%)": 15,
+    "Good (18%)": 18,
+    "Excellent (20%)": 20
+  }
 
+  tipDetails = {
+    serviceCost: 0,
+    serviceQuality: this.serviceQualityOptions["Okay (15%)"]
+  }
+
+  onSubmit(form: any): void {
+    if (form.valid) {
+      console.log(this.tipDetails)
+    }
+  }
 }
