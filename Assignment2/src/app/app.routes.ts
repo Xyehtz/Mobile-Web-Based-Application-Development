322950448755c715b0db7de1606240ca4874c4a5
Alejandro Garzon <alejitogarbo@gmail.com>
2024-11-02 20:04:39 -0400

    created basic router and results component

--- Assignment2/src/app/app.routes.ts
+++ Assignment2/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {FormComponent} from './form/form.component';
+import {ResultsComponent} from './results/results.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'tipCalculator', component: FormComponent},
+  {path: '', redirectTo: 'tipCalculator', pathMatch: 'full'},
+  {path: 'tipResults', component: ResultsComponent}
+];
--- Assignment2/src/app/form/form.component.ts
+++ Assignment2/src/app/form/form.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import {FormsModule, NgForm} from '@angular/forms';
 import {KeyValuePipe, NgForOf, NgIf} from '@angular/common';
+import {Router} from '@angular/router';
+import {HeaderComponent} from '../header/header.component';
+import {FooterComponent} from '../footer/footer.component';
 
 @Component({
   selector: 'app-form',
@@ -9,12 +12,16 @@ import {KeyValuePipe, NgForOf, NgIf} from '@angular/common';
     FormsModule,
     NgIf,
     NgForOf,
-    KeyValuePipe
+    KeyValuePipe,
+    HeaderComponent,
+    FooterComponent
   ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+  constructor(private router: Router) { }
+
   serviceQualityOptions: {[key: string]: number} = {
     "Okay (15%)": 15,
     "Good (18%)": 18,
@@ -30,6 +37,7 @@ export class FormComponent {
   onSubmit(form: any): void {
     if (form.valid) {
       console.log(this.tipDetails)
+      this.router.navigate(['tipResults', {state: {data: this.tipDetails}}])
     }
   }
 }
